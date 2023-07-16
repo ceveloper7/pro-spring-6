a85800bec4365b64209007f162a53b92b933af91
cevadev <carlos.villanueva.altuna@gmail.com>
2023-07-16 20:01:03 -0300

    Curso Pro Spring 6 - Chapter 02 - Spring XML Config

---
+++ capitulo2/src/main/java/com/ceva/prospring6/two/decoupled/HelloWorldSpringDI.java
@@ -0,0 +1,12 @@
+package com.ceva.prospring6.two.decoupled;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloWorldSpringDI {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/app-context.xml");
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
+    }
+}
