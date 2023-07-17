c46784c0395db6033600ec250bd707373f259349
cevadev <carlos.villanueva.altuna@gmail.com>
2023-07-17 02:02:36 -0300

    Curso Pro Spring 6 - Chapter 02 - Spring Annotation Config

---
+++ capitulo2/src/main/java/com/ceva/prospring6/two/annotated/HelloWorldSpringAnnotated.java
@@ -0,0 +1,13 @@
+package com.ceva.prospring6.two.annotated;
+
+import com.ceva.prospring6.two.decoupled.MessageRenderer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class HelloWorldSpringAnnotated {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
+    }
+}
